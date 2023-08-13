3d3a2096e95fab2a9be93aefa1c3ba7155321f48
ziani-hamza <zianih41@gmail.com>
2023-08-13 23:38:11 +0100

    third

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,13 +7,13 @@ export default class Porfolio extends Component {
       <div className="row">
         <div className="twelve columns collapsed">
           <h1>Check Out Some of My Works.</h1>
+                            <a href="https://youtu.be/SRuFTzWNo58" target="_blank" rel="noopener noreferrer">
+                              <h5 style={{ textDecoration: 'underline', cursor: 'pointer' }}>The Youtube Channel</h5>
+                            </a>
           <a href="https://docs.google.com/presentation/d/1O2NBR9hRGwTIfIYx4UdRWfrskW6ENQ8ZLsX1W8g5S5U/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
-                              <h5>Projet 1</h5>
+                              <h5 style={{ textDecoration: 'underline', cursor: 'pointer' }}>Project</h5>
                             </a>
                           
-                            <a href="https://youtu.be/SRuFTzWNo58" target="_blank" rel="noopener noreferrer">
-                              <h5>Projet 2</h5>
-                            </a>
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
           {
             resumeData.portfolio && resumeData.portfolio.map((item)=>{
