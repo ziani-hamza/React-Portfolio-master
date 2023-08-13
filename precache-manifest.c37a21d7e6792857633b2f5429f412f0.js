2fd5ad76802903200710be8dffb22ac9c843b37f
ziani-hamza <zianih41@gmail.com>
2023-08-13 22:40:32 +0100

    Updates

---
+++ precache-manifest.c37a21d7e6792857633b2f5429f412f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c95e981e15877e858b0",
+    "url": "/React-Portfolio-master/static/css/main.9d61e428.chunk.css"
+  },
+  {
+    "revision": "7c95e981e15877e858b0",
+    "url": "/React-Portfolio-master/static/js/main.7c95e981.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio-master/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ae188cb077ba5f3ea0f4",
+    "url": "/React-Portfolio-master/static/js/runtime~main.ae188cb0.js"
+  },
+  {
+    "revision": "0b9b2ec8ba2033aae7d4b31c849001d7",
+    "url": "/React-Portfolio-master/index.html"
+  }
+];
