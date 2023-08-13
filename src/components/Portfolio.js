cc3930bd6cb5ee786fd7e0652a2b501ed52a261e
ziani-hamza <zianih41@gmail.com>
2023-08-13 22:04:55 +0100

    first commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Works.</h1>
+          <a href="https://docs.google.com/presentation/d/1O2NBR9hRGwTIfIYx4UdRWfrskW6ENQ8ZLsX1W8g5S5U/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+                              <h5>Projet 1</h5>
+                            </a>
+                          
+                            <a href="https://youtu.be/SRuFTzWNo58" target="_blank" rel="noopener noreferrer">
+                              <h5>Projet 2</h5>
+                            </a>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <a src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,42 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "mohamed amine el mounkir",
+    "role": "Software Engineer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mohamed-amine-el-mounkir-8421b1264/",
+          "className":"fa fa-linkedin"
+        },
+      
+      ],
+    "aboutme":"EL mounkir mohamed amine is a dynamic and innovative person he has a critical thinking and he loves solve problems also he is an online seller and founder of some small online business like BOUTIQUE CS,e-jewelrry.com .He’s passionate about marketing and coding so he has established him self as a great student in software engineer at alx ,his goal is to become a software engineer because he loves this field . Before that he helps to build an official website  professional journey : Elmounkir mohamed amine began with a bachelor’s degree in business management and he did some internship after that he decides to change his field into software engineer because he discover his passion and what he loves to do ",
+    "address":"Morocco",
+    
+   
+   
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/modales/coffee.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"My Elevator Pitch",
+        "name":"Some technical guy"
+      },
+    
+    ]
+  }
+  
+  export default resumeData
